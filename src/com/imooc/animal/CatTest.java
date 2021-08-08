8e40a16c05039cec1f6636dc20e6604199604396
yelan <1845480035@qq.com>
2021-08-08 22:01:22 +0800

    11-1-10 new关键字（上）【11-1完结】

--- src/com/imooc/animal/CatTest.java
+++ src/com/imooc/animal/CatTest.java
@@ -5,16 +5,26 @@ package com.imooc.animal;
 public class CatTest {
     public static void main(String[] args) {
         Cat one=new Cat();
+        Cat two=new Cat();
         one.eat();
         one.run();
         one.name = "花花";
         one.month = 2;
         one.weight = 1000;
         one.species = "英国短毛猫";
+        two.name="凡凡";
+        two.month = 1;
+        two.weight=800;
+        two.species = "中华田园猫";
         System.out.println("昵称：" + one.name);
         System.out.println("年龄：" + one.month);
         System.out.println("体重：" + one.weight);
         System.out.println("品种：" + one.species);
-        one.run(one.name);
+        System.out.println("---------------------------");
+        System.out.println("昵称：" + two.name);
+        System.out.println("年龄：" + two.month);
+        System.out.println("体重：" + two.weight);
+        System.out.println("品种：" + two.species);
+        //one.run(one.name);
     }
 }
